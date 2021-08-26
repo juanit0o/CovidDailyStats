5b44726eafb5eb6df053800239fdbb28462288a9
João Funenga <fc53504@alunos.fc.ul.pt>
2021-08-26 18:47:32 +0100

    Python script added along with the notification icon

---
+++ covidStats.py
@@ -0,0 +1,32 @@
+import time #To make it run daily
+from datetime import date #To read current date
+import requests as req #To request covid api
+from plyer import notification #For notifications
+
+
+data = None
+try:
+    data = req.get("https://corona-rest-api.herokuapp.com/Api/portugal")
+    if(data != None):
+        dataJSON = data.json().values()
+        val_iter = iter(dataJSON)
+        value = next(val_iter)
+        print(value)
+
+        
+        while(True):
+            today = date.today()
+            statusDay = "Covid19 Stats on " + today.strftime("%d/%m/%Y")
+            notification.notify(
+                title = statusDay, 
+                message = "Total Cases: " + str(value['cases']) + 
+                ", Cases Today: " +  str(value['todayCases']) + 
+                ", Deaths Today: " + str(value['todayDeaths']) +
+                ", Active Cases: " + str(value['active']),
+                app_icon = "covid.ico",
+                timeout = 30)
+            time.sleep(60*60*4)
+except Exception as e:
+    #Request Failed
+    print(e)
+    print("Request failed, try again later")
